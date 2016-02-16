018afa1b3dd7fefe4feeb6fd9ddce9b05003adf5
Rachel Black <rachel.black1984@googlemail.com>
2016-02-16 11:32:27 +0000

    inital commit first test set up, looking to immutable data

---
+++ test/core_spec.js
@@ -0,0 +1,27 @@
+import {List, Map} from 'immutable';
+import {expect} from 'chai';
+import {setEntries} from '../src/core';
+
+describe('application logic', ()=> {
+  describe('setEntries', ()=> {
+
+    it('adds the entries to the state', ()=>{
+      const state = Map();
+      const entries = List.of('Trainspotting', '28 Days Later');
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries: List.of('Trainspotting', '28 Days Later')
+      }));
+    });
+
+    //checking our array turns in to an immutable list form immutable.js library
+    it ('converts to immutable', ()=>{
+      const state = Map();
+      const entries = ['Trainspotting', '28 Days Later'];
+      const nextState = setEntries(state, entries);
+      expect(nextState).to.equal(Map({
+        entries : List.of('Trainspotting', '28 Days Later')
+      }));
+    })
+  });
+});
